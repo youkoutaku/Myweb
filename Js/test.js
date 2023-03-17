ddd4ac5291200c474150c1c62acfe951c7a7a353
youkoutaku <87299911+youkoutaku@users.noreply.github.com>
2023-03-17 10:21:25 +0900

    3/17/2023

---
+++ Js/test.js
@@ -0,0 +1,6 @@
+var access =
+	document.getElementById("myname");
+//使用document对象中的getElementById的方法，获取id为code9的元素，把这个元素赋给access
+var code = access.innerHTML;
+//通过access中的innerHTML的方法，获取它的文本内容，赋给code
+alert(code);
